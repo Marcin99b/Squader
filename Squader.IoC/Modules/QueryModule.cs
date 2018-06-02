f4b2a1a1dee9b8869341d5923541347ee1bb3211
livaen <darius2424@gmail.com>
2018-06-02 16:56:51 +0200

    Update Query Autofac

--- Squader.IoC/Modules/QueryModule.cs
+++ Squader.IoC/Modules/QueryModule.cs
@@ -1,44 +1,57 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using Autofac;
 using Squader.Cqrs;
+//using Squader.ReadModel;
 
 namespace Squader.IoC.Modules
 {
     public class QueryModule :Autofac.Module
     {
+        
         protected override void Load(ContainerBuilder builder)
         {
-            base.Load(builder);
+            var list = new List<string>();
+            var stack = new Stack<Assembly>();
 
-            var handlers = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes())
-                .Where(b => typeof(IQueryHandler).IsAssignableFrom(b) && !b.IsInterface && !b.IsAbstract)
-                .ToList();
-
-            foreach (var handler in handlers)
+            stack.Push(Assembly.GetEntryAssembly());
+            do
             {
-                builder.RegisterType(handler)
-                    .As(handler.GetInterfaces().First())
-                    .InstancePerLifetimeScope();
-            }
-            
-            builder.Register<Func<Type, IQueryHandler>>(c =>
-            {
-                var ctx = c.Resolve<IComponentContext>();
+                var asm = stack.Pop();
 
-                return t =>
+                if (asm.FullName.Contains("Squader.ReadModel"))
                 {
-                    var handlerType = typeof(IQueryHandler<,>).MakeGenericType(t);
-                    return (IQueryHandler)ctx.Resolve(handlerType);
-                };
-            });
+                    var handlers = asm.GetTypes()
+                        .Where(type => typeof(IQueryHandler).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+                        .ToList();
+
+                    foreach (var handler in handlers)
+                    {
+                        builder.RegisterType(handler)
+                            .As(handler.GetInterfaces().First())
+                            .InstancePerLifetimeScope();
+                    }
+                }
+
+                foreach (var reference in asm.GetReferencedAssemblies())
+                    if (!list.Contains(reference.FullName))
+                    {
+                        stack.Push(Assembly.Load(reference));
+                        list.Add(reference.FullName);
+                    }
+
+            }
+            while (stack.Count > 0);
+             
 
             builder.RegisterType<QueryBus>()
-                .AsImplementedInterfaces()
+                .As<IQueryBus>()
                 .InstancePerLifetimeScope();
             
         }
-    }
         
-    
+    }
 }
