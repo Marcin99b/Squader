a6cc4400fc26c5ae9cb743a09bee5f8eb90bc1c7
livaen <darius2424@gmail.com>
2018-06-09 22:02:29 +0200

    Add Authentication Controller/ UserModel

---
+++ src/Squader.Api/Areas/Authentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Squader.Cqrs;
+
+namespace Squader.Api.Areas.Authentication.Controllers
+{
+    public class AuthenticationController : BaseApiController
+    {
+        public AuthenticationController(ICommandBus commandBus, IQueryBus queryBus, ILogger<AuthenticationController> logger) : base(commandBus, queryBus, logger)
+        {
+
+        }
+        
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/Squader.DomainModel/Users/User.cs
+++ src/Squader.DomainModel/Users/User.cs
@@ -6,5 +6,10 @@ namespace Squader.DomainModel.Users
 {
     public class User
     {
+        public Guid Id { get; private set; }
+        public string Username { get; private set; }
+        public string Email { get; set; }
+        public string Password { get; protected set; }
+        public string Salt { get; protected set; }
     }
 }
