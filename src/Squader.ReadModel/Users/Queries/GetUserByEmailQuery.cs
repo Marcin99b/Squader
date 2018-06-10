f0f9f31f5c0b20aa01db18c5ab2d9f54a140e368
livaen <darius2424@gmail.com>
2018-06-10 19:17:41 +0200

    Add Queries

---
+++ src/Squader.ReadModel/Users/Queries/GetUserByEmailQuery.cs
@@ -0,0 +1,12 @@
+﻿using Squader.Cqrs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Squader.ReadModel.Users.Queries
+{
+    class GetUserByEmailQuery :IQuery
+    {
+
+    }
+}
