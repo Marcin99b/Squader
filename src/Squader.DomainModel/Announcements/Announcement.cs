dc7962cf7eb5ba3ea43ce20c24d4b16dbfb5e85d
Marcin Bigos <marcin.bigos99@gmail.com>
2018-06-15 23:18:50 +0200

    create announcement object with getters and setters

--- src/Squader.DomainModel/Announcements/Announcement.cs
+++ src/Squader.DomainModel/Announcements/Announcement.cs
@@ -1,18 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
+using Squader.DomainModel.Users;
 
 namespace Squader.DomainModel.Announcements
 {
     public class Announcement
     {
-        public Guid Id { get; private set; }
-        public string Title { get; private set; }
-        public string Description { get; private set; }
+        private ISet<string> requirements = new HashSet<string>();
+        private ISet<string> tags = new HashSet<string>();
 
-        public Announcement(string title, string description)
+        public Guid Id { get; private set; }
+        public Guid AuthorId { get; private set; }
+        public Guid TeamId { get; private set; } //TODO add team domain model
+        public string Title { get; private set; }
+        public string ShortDescription { get; private set; }
+        public string Description { get; private set; }
+        public IEnumerable<string> Requirements
+        {
+            get => requirements;
+            private set => requirements = new HashSet<string>(value);
+        }
+        public IEnumerable<string> Tags
+        {
+            get => tags;
+            private set => tags = new HashSet<string>(value);
+        }
+        public DateTime ChangedAt { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        public Announcement(User author, string title, string shortDescription, string description)
         {
             Id = Guid.NewGuid();
-            Title = title;
-            Description = description;
+            AuthorId = author.Id;
+            SetTitle(title);
+            SetShortDescription(shortDescription);
+            SetDescription(description);
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public Announcement(User author, string title, string shortDescription, string description, IEnumerable<string> requirements, IEnumerable<string> tags)
+        {
+            Id = Guid.NewGuid();
+            AuthorId = author.Id;
+            SetTitle(title);
+            SetShortDescription(shortDescription);
+            SetDescription(description);
+            SetRequirements(requirements);
+            SetTags(tags);
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void SetTitle(string title)
+        {
+            this.Title = title;
+            UpdateVersion();
+        }
+
+        public void SetShortDescription(string shortDescription)
+        {
+            this.ShortDescription = shortDescription;
+            UpdateVersion();
+        }
+
+        public void SetDescription(string description)
+        {
+            this.Description = description;
+        }
+
+        public void SetRequirements(IEnumerable<string> requirements)
+        {
+            Requirements = requirements;
+            UpdateVersion();
+        }
+
+        public void SetRequirements(Action<IEnumerable<string>> action)
+        {
+            action.Invoke(Requirements);
+            UpdateVersion();
+        }
+
+        public void SetTags(Action<IEnumerable<string>> action)
+        {
+            action.Invoke(Tags);
+            UpdateVersion();
+        }
+
+        public void SetTags(IEnumerable<string> tags)
+        {
+            Tags = tags;
+            UpdateVersion();
+        }
+
+        public void UpdateVersion()
+        {
+            ChangedAt = DateTime.UtcNow;
+        }
+
+        public void Delete()
+        {
+            IsDeleted = true;
         }
     }
 }
