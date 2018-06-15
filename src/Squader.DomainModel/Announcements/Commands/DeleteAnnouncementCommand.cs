1bcab3ff26417952b48964a42e1595a0666ac868
Marcin Bigos <marcin.bigos99@gmail.com>
2018-06-16 00:13:39 +0200

    add delete announcement command-handler

---
+++ src/Squader.DomainModel/Announcements/Commands/DeleteAnnouncementCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Squader.Cqrs;
+
+namespace Squader.DomainModel.Announcements.Commands
+{
+    public class DeleteAnnouncementCommand : ICommand
+    {
+        public Guid AnnouncementId { get; private set; }
+
+        public DeleteAnnouncementCommand(Guid announcementId)
+        {
+            AnnouncementId = announcementId;
+        }
+    }
+}
