da5f6591c3d1ed3321762b12c8122cde207335d7
Mateusz Stepaniuk <stepek75@gmail.com>
2018-06-15 16:35:46 +0200

    Changed placing of language switcher

--- src/Squader.ClientApp/src/App.vue
+++ src/Squader.ClientApp/src/App.vue
@@ -3,7 +3,16 @@
     <router-view/>
   </div>
 </template>
-
+<script>
+  export default {
+    name: 'App',
+    methods: {
+      changeLang(lang) {
+        this.$store.dispatch('changeLanguage', lang);
+      }
+    }
+  }
+</script>
 <style>
 
 </style>
--- src/Squader.ClientApp/src/views/Home.vue
+++ src/Squader.ClientApp/src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home">
-    <div class="tekst">
-      {{ 'Hello' | translate}}
-    </div>
-    <button @click="changeLang('en')">Translate into English</button>
-    <button @click="changeLang('pl')">Przetłumacz na Polski</button>
-    <button @click="changeLang('de')">Auf Deutsch übersetzen</button>
+
   </div>
 </template>
 
@@ -14,9 +9,7 @@
 export default {
   name: "home",
   methods:{
-    changeLang(lang) {
-      this.$store.dispatch('changeLanguage', lang);
-    }
+
   }
 };
 </script>
