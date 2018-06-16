5d32545846f5e3cbb28fb82142dc5c487903a036
Marcin Bigos <marcin.bigos99@gmail.com>
2018-06-16 14:42:14 +0200

    add userteam object without constructor

---
+++ src/Squader.DomainModel/Teams/UserTeam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Squader.DomainModel.Teams
+{
+    public class UserTeam
+    {
+        public Guid UserId { get; private set; }
+        public string TeamRole { get; private set; }
+        public DateTime ChangedAt { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}
