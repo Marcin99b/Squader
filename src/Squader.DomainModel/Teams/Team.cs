5b6f4f580db2b13d1d9f7d3f9c4b233370d008e9
Marcin Bigos <marcin.bigos99@gmail.com>
2018-06-16 14:42:37 +0200

    add team object without constructor, with team users

--- src/Squader.DomainModel/Teams/Team.cs
+++ src/Squader.DomainModel/Teams/Team.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Squader.DomainModel.Teams
 {
     public class Team
     {
+        private ISet<UserTeam> users = new HashSet<UserTeam>();
+
         public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public IEnumerable<UserTeam> Users
+        {
+            get => users;
+            private set => users = new HashSet<UserTeam>(value);
+        }
     }
 }
