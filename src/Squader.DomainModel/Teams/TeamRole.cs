01a5a0214b9c3108c69a216cb858a341edf1de8b
Marcin Bigos <marcin.bigos99@gmail.com>
2018-06-16 14:51:04 +0200

    add team role object

---
+++ src/Squader.DomainModel/Teams/TeamRole.cs
@@ -0,0 +1,34 @@
+﻿namespace Squader.DomainModel.Teams
+{
+    public class TeamRole
+    {
+        public string Role { get; private set; }
+        public bool ChangeUsersRoles { get; private set; }
+        public bool DeleteUsers { get; private set; }
+        public bool DeleteTeam { get; private set; }
+
+        public static TeamRole Owner = new TeamRole
+        {
+            Role = "Owner",
+            ChangeUsersRoles = true,
+            DeleteUsers = true,
+            DeleteTeam = true
+        };
+
+        public static TeamRole Admin = new TeamRole
+        {
+            Role = "Admin",
+            ChangeUsersRoles = true,
+            DeleteUsers = true,
+            DeleteTeam = false
+        };
+
+        public static TeamRole User = new TeamRole
+        {
+            Role = "User",
+            ChangeUsersRoles = false,
+            DeleteUsers = false,
+            DeleteTeam = false
+        };
+    }
+}
