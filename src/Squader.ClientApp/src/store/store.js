16dbc415403d9c26e7f14c0586b875b617e966ab
Borys Tymiński <tyminski.borys@gmail.com>
2018-06-19 00:37:36 +0200

    add login/logout mutations and state to store.js

--- src/Squader.ClientApp/src/store/store.js
+++ src/Squader.ClientApp/src/store/store.js
@@ -1,28 +1,45 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import vuexI18n from 'vuex-i18n';
-
+import vuexI18n from "vuex-i18n";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
-
 const store = new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {
-        changeLanguage(ctx, lang){
-            Vue.i18n.set(lang);
-        }
+  state: {
+    logged: false,
+    token: ""
+  },
+  mutations: {
+    login: (state, response) => {
+      if (response) {
+        state.logged = true;
+        state.token = response;
+        console.log("state.logged flag is: " + state.logged);
+        console.log("state.token: " + state.token);
+      }
+    },
+    logout: state => {
+      state.logged = false;
+      state.token = "";
     }
+  },
+  actions: {
+    changeLanguage(ctx, lang) {
+      Vue.i18n.set(lang);
+    }
+  },
+  plugins: [createPersistedState()]
 });
 
 Vue.use(vuexI18n.plugin, store);
 
+//Do refactor here we do not want this file large
 const translationsPl = {
-    "Hello": "Cześć"
+  Hello: "Cześć"
 };
 
-Vue.i18n.add('pl', translationsPl);
+Vue.i18n.add("pl", translationsPl);
 Vue.i18n.set(window.navigator.language);
 
 export default store;
