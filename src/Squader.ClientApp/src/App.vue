8837d5b39f86eb8edc5bc0677af2b870ca41fb11
Borys Tymiński <tyminski.borys@gmail.com>
2018-06-19 03:22:15 +0200

    add register view, register in router and temporary router link in app.vue

--- src/Squader.ClientApp/src/App.vue
+++ src/Squader.ClientApp/src/App.vue
@@ -4,26 +4,30 @@
     <router-link to="/login">
       Login link na razie tutaj bo potem chyba w header bedzie ale to robi @Mateusz więc sie nie wpycham
     </router-link>
+    <br>
+    <router-link to="/register">
+      Register link na razie tutaj bo potem chyba w header bedzie ale to robi @Mateusz więc sie nie wpycham
+    </router-link>
     <router-view/>
   </div>
 </template>
 <script>
-  import Header from './components/Header';
+import Header from "./components/Header";
 
-  export default {
-    name: 'App',
-    componets: {
-      Header
-    },
-    methods: {
-      changeLang(lang) {
-        this.$store.dispatch('changeLanguage', lang);
-      }
+export default {
+  name: "App",
+  components: {
+    Header
+  },
+  methods: {
+    changeLang(lang) {
+      this.$store.dispatch("changeLanguage", lang);
     }
   }
+};
 </script>
 <style>
-:root{
+:root {
   --main-color: #3086ff;
   --secondary-color: #014fa8;
   --background-grey: #1e1e1e;
