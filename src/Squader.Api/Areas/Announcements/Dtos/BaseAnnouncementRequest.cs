87e50d9417d41e97e2a90ab29151abe0c9bf67d3
Marcin Bigos <marcin.bigos99@gmail.com>
2018-07-16 05:20:46 +0200

    create base announcement request

---
+++ src/Squader.Api/Areas/Announcements/Dtos/BaseAnnouncementRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Squader.Api.Areas.Announcements.Dtos
+{
+    public class BaseAnnouncementRequest
+    {
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<string> Requirements { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+    }
+}
