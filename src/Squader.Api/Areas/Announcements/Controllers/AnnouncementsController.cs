67ac5d6a32a5ee5dedd890db16adc819ea020213
Marcin Bigos <marcin.bigos99@gmail.com>
2018-07-16 05:24:48 +0200

    create announcements controller with CRUD

--- src/Squader.Api/Areas/Announcements/Controllers/AnnouncementsController.cs
+++ src/Squader.Api/Areas/Announcements/Controllers/AnnouncementsController.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Squader.Api.Areas.Announcements.Dtos;
 using Squader.Cqrs;
 using Squader.DomainModel.Announcements.Commands;
+using Squader.ReadModel.Announcements.Queries;
 
 namespace Squader.Api.Areas.Announcements.Controllers
 {
@@ -13,6 +16,40 @@ namespace Squader.Api.Areas.Announcements.Controllers
         {
             
         }
-        
+
+        [HttpGet("{id}")]
+        public IActionResult GetAccouncementById(Guid id)
+        {
+            var query = new GetAnnouncementByIdQuery(id);
+            var announcement = queryBus.Execute(query).Announcement;
+            var announcementDto = new AnnouncementDto(announcement);
+            return Json(announcementDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewAnnouncementAsync([FromBody] CreateNewAnnouncementRequest request)
+        {
+            var command = new CreateNewAnnouncementCommand(request.AuthorId, request.Title, request.ShortDescription,
+                request.Description, request.Requirements, request.Tags);
+            await commandBus.ExecuteAsync(command);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAnnouncementAsync([FromBody] UpdateAnnouncementRequest request)
+        {
+            var command = new UpdateAnnouncementCommand(request.AnnouncementId, request.Title, request.ShortDescription,
+                request.Description, request.Requirements, request.Tags);
+            await commandBus.ExecuteAsync(command);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAnnouncementAsync([FromBody] DeleteAnnouncementRequest request)
+        {
+            var command = new DeleteAnnouncementCommand(request.AnnouncementId);
+            await commandBus.ExecuteAsync(command);
+            return Ok();
+        }
     }
 }
