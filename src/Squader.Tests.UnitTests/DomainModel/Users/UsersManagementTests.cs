91e136b0b2f6ab78b679fb3508f0f77bcfe3a40f
livaen <darius2424@gmail.com>
2018-07-17 23:09:49 +0200

    Create updateUser unit test

--- src/Squader.Tests.UnitTests/DomainModel/Users/UsersManagementTests.cs
+++ src/Squader.Tests.UnitTests/DomainModel/Users/UsersManagementTests.cs
@@ -29,20 +29,19 @@ namespace Squader.Tests.UnitTests.DomainModel.Users
                 .Returns(Task.CompletedTask);
 
             var registerUserHandler = new RegisterUserHandler(usersRepository.Object);
-            var registerUserCommand = new RegisterUserCommand("test", "Test@test.pl", "test", "test");
+            var command = new RegisterUserCommand("test", "Test@test.pl", "test", "test");
 
             //Act
-            await registerUserHandler.HandleAsync(registerUserCommand);
+            await registerUserHandler.HandleAsync(command);
 
             //Assert
-            Assert.That(createdUser.Username, Is.EqualTo(registerUserCommand.Username));
+            Assert.That(createdUser.Username, Is.EqualTo(command.Username));
         }
 
         [Test]
         public async Task ShouldDeleteUserAccountCorrectly()
         {
             //Arrange
-
             User user = new User("test", "test", "test", "test");
             var dateDeleted = new DateTime();
             var usersRepository = new Mock<IUsersRepository>();
@@ -69,26 +68,51 @@ namespace Squader.Tests.UnitTests.DomainModel.Users
             usersRepository.Verify(x => x.GetAsync(It.IsAny<Guid>()), Times.Once);
             usersRepository.Verify(x => x.UpdateAsync(It.IsAny<User>()), Times.Once);
             Assert.That(user.IsDeleted);
-            Assert.That(user.ChangedAt, Is.EqualTo(dateDeleted));
-            
-        } 
+            Assert.That(user.ChangedAt, Is.EqualTo(dateDeleted));           
+        }
 
-        
+        [Test]
+        public async Task ShouldUpdateUserAccountCorrectly()
+        {
+            //Arrange
+            User user = new User("test", "test", "test", "test");
+            var usersRepository = new Mock<IUsersRepository>();
+            var dateUpdated = new DateTime();
+
+            usersRepository.Setup(x => x.GetAsync(It.IsAny<Guid>()))
+                .Returns(Task.FromResult(user));
+
+            usersRepository.Setup(x => x.UpdateAsync(It.IsAny<User>()))
+                .Callback<User>(x =>
+                {
+                    user = x;
+                    dateUpdated = user.ChangedAt;
+                })
+                .Returns(Task.CompletedTask);
+
+            var updateUserHandler = new UpdateUserHandler(usersRepository.Object);
+            var command = new UpdateUserCommand(user.Id, "test2", "test2", "test2", "test2","test2");
+
+            //Act
+            await updateUserHandler.HandleAsync(command);
+
+
+            //Assert
+            usersRepository.Verify(x => x.GetAsync(It.IsAny<Guid>()), Times.Once);
+            usersRepository.Verify(x => x.UpdateAsync(It.IsAny<User>()), Times.Once);
+            Assert.That(user.ChangedAt, Is.EqualTo(dateUpdated));
+            Assert.That(user.Username, Is.EqualTo(command.Username));
+            Assert.That(user.Email, Is.EqualTo(command.Email));
+            Assert.That(user.Forename, Is.EqualTo(command.Forename));
+            Assert.That(user.Surname, Is.EqualTo(command.Surname));
+            Assert.That(user.City, Is.EqualTo(command.City));
+        }
 
         //private User GetLastInputUser(Mock<IUsersRepository> usersRepository)
         //    => (User) usersRepository.Invocations.Select(inv => inv.Arguments)
         //        .Select(obj =>obj.FirstOrDefault(x => x.GetType() == typeof(User))).First();
 
-        //private Mock<IUsersRepository> configureRepository(Mock<IUsersRepository> usersRepository)
-        //{
-        //    usersRepository.Setup(x => x.AddAsync(It.IsAny<User>()))
-        //        .Callback<User>(x => user = x)
-        //        .Returns(Task.CompletedTask);
-
-        //    usersRepository.Setup(x => x.GetAsync(It.IsAny<Guid>()))
-        //        .Returns(Task.FromResult(user));
-
-        //    return usersRepository;
-        //}
+ 
     }
 }
+
