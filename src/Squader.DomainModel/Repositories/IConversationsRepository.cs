a3af65dee6a96b28b6ed75de4fe974adaa98c063
Marcin Bigos <marcin.bigos99@gmail.com>
2018-07-28 02:23:56 +0200

    add IConversationsRepository

---
+++ src/Squader.DomainModel/Repositories/IConversationsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Squader.DomainModel.Conversations;
+using System;
+using System.Threading.Tasks;
+
+namespace Squader.DomainModel.Repositories
+{
+    public interface IConversationsRepository : IRepository
+    {
+        Task AddAsync(Conversation conversation);
+        Conversation Get(Guid conversationId);
+        Task UpdateAsync(Conversation conversation);
+    }
+}
